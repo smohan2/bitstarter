12edd459a13a8b65fbbd114fff844406312b4749
Surya Mohan <mohan.surya@gmail.com>
2013-07-25 01:32:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var FILE_DEFAULT = "index.html";
 
+var message = function (file) {
+file = file || FILE_DEFAULT;
+var buffer = new Buffer(fs.readFileSync(file));
+return (buffer.toString());
+}
+
+sometext = 'TEXT'
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+  response.send(sometext +'');
 });
