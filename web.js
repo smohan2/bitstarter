5e07b2c6785b494fdb914a88a6d6c444da861d10
Surya Mohan <mohan.surya@gmail.com>
2013-07-25 01:40:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,3 +6,12 @@ file = file || FILE_DEFAULT;
 var buffer = fs.readFileSync(file);
 return (buffer.toString('utf-8'));
 }
+
+app.get('/', function(request,response){
+response.send(message());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log("Listening on " + port);
+});
