c7d8141cf2ffdb4599563d597604c3978f32615e
Surya Mohan <mohan.surya@gmail.com>
2013-07-26 18:44:06 +0000

    Updated web.js to deploy bitstarter final

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var content = fs.readFileSync('index.html','utf-8');
+var content = fs.readFileSync('bitstarter-hw4.html','utf-8');
 
 app.get('/', function(request,response){
 response.send(content);
